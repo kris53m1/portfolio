601afa0d1cb88ad64e81c0b77c85fdec4add4676
Pablo Esco <pabloesco@MacBook-Air-tilhrende-Pablo.local>
2021-01-13 02:47:35 +0100

    font sat ind

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 console.log("burgermenu");
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
-
+console.log("test1");
 const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 
-console.log("test");
+console.log("test2");
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active")
 })
+
+console.log("test3");
